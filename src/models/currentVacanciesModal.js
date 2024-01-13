623fe6ac0761df9ef9518da92cb96e5a860d8b4b
Shivkumar Sahu <meghasahu29june2001@gmail.com>
2024-01-13 18:02:31 +0530

    klimart project

---
+++ src/models/currentVacanciesModal.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const currentVacanciesSchema = new mongoose.Schema({
+    role : {type : String},
+    experience : {type : String},
+    location : {type : String}
+
+},{timestamps : true})
+module.exports = mongoose.model('currentVacancies' ,currentVacanciesSchema, 'currentVacancies')
